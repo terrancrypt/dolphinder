c4dbef58f2a97ee86785fa24683aeb1d31c69df1
cuongtq <cuongtqse180377@fpt.edu.vn>
2025-09-20 12:15:51 +0700

    add profile

--- src/data/devs.ts
+++ src/data/devs.ts
@@ -11,13 +11,13 @@ type Dev = {
 
 const DEV_DATA: Dev[] = [
     {
-        name: "Hulk",
-        username: "hulk",
-        avatar: "/avatar/hulk-avatar.png",
-        github: "https://github.com/Huc06",
-        linkedin: "https://www.linkedin.com/in/hulk-phuc-ha-84a685301/",
+        name: "Trần Quốc Cường",
+        username: "rosettyluvhenny@gmail.com",
+        avatar: "/data/images.png",
+        github: "https://github.com/Rosettyluvhenny",
+        linkedin: "https://www.linkedin.com/in/c%C6%B0%E1%BB%9Dng-tr%E1%BA%A7n-5bb31225b/",
         bio: "This product offers a unique solution to a common problem.",
-        slushWallet: "0xaea6f58e4261b34032934c6c20e0b5b4fd94bac2e86734e3f2d3d5856b443108",
+        slushWallet: "0x6d3bf16e92bbb9841f5f931d8cf6a2b5aaca13a9be6a20206e5971a910258d6b",
     },
 ];
 
